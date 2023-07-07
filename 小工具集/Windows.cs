2b1b5271e3143a5d8312ca0dd9cc4f91b4385f06
44578287 <g9964957@gmail.com>
2023-07-08 04:01:27 +0800

    添加项目文件。

---
+++ 小工具集/Windows.cs
@@ -0,0 +1,33 @@
+﻿namespace 小工具集
+{
+    public class Windows
+    {
+        /// <summary>
+        /// 从捷径中获取目标路径
+        /// </summary>
+        /// <param name="Path">捷径文件路径</param>
+        /// <returns>目标文件路径</returns>
+        public static string? LnkToPath(string Path)
+        {
+            string? targetPath = null;
+            try
+            {
+                if (System.IO.File.Exists(Path))
+                {
+                    IWshRuntimeLibrary.WshShell shell = new();
+                    IWshRuntimeLibrary.IWshShortcut shortcut = (IWshRuntimeLibrary.IWshShortcut)shell.CreateShortcut(Path);
+                    targetPath = shortcut.TargetPath;
+                }
+                else
+                {
+                    throw new Exception("文件不存在!");
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return targetPath;
+        }
+    }
+}
---
+++ 工具测试/Program.cs
@@ -0,0 +1,3 @@
+﻿using static 小工具集.Windows;
+
+//Console.WriteLine(LnkToPath(@"C:\Users\g9964\Desktop\阿里巴巴DNS检测工具.lnk"));
