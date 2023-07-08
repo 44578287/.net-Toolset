126b1ca90c21f8d2f0121f613ef196cdbcc29be1
44578287 <g9964957@gmail.com>
2023-07-08 18:05:35 +0800

    File-添加万能解压 Network-修改文件下载并添加带进度显示 Text-添加单位自动转换

--- 小工具集/Windows.cs
+++ 小工具集/Windows.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.NetworkInformation;
+﻿using System.Diagnostics;
+using System.Net.NetworkInformation;
+using SharpCompress.Archives;
+using SharpCompress.Common;
 
 namespace 小工具集
 {
@@ -151,6 +154,61 @@ namespace 小工具集
 
                 return watcher;
             }
+            /// <summary>
+            /// 万能解压
+            /// </summary>
+            /// <param name="ZipPath">压缩包位置</param>
+            /// <param name="ObjectivePath">解压位置(未指定则仅检视压缩包内容)</param>
+            /// <param name="Pwd">压缩包密码</param>
+            /// <param name="Overwrite">覆盖目标文件</param>
+            /// <returns>文件列表</returns>
+            public static Dictionary<string, IArchiveEntry> Unzip(
+                string ZipPath,
+                string? ObjectivePath = null,
+                string? Pwd = null,
+                bool Overwrite = true
+                )
+            {
+                Dictionary<string, IArchiveEntry> TempData = new();
+                if (!System.IO.Directory.Exists(ObjectivePath) & ObjectivePath != null)//自动创建解压文件夹
+                {
+                    Directory.CreateDirectory(ObjectivePath!);
+                }
+                using (var stream = System.IO.File.OpenRead(ZipPath))//读取压缩文件
+                {
+                    SharpCompress.Readers.ReaderOptions? ReaderOptions = new()
+                    {
+                        LeaveStreamOpen = true, // 保持流打开，以便在解压缩后删除源文件
+                        Password = Pwd // 设置密码
+                    };
+                    using (SharpCompress.Archives.IArchive? archive = SharpCompress.Archives.ArchiveFactory.Open(stream, ReaderOptions))
+                    {
+                        if (ObjectivePath != null)
+                        {
+                            foreach (SharpCompress.Archives.IArchiveEntry entry in archive.Entries)
+                            {
+                                if (!entry.IsDirectory)
+                                {
+                                    entry.WriteToDirectory(ObjectivePath, new ExtractionOptions
+                                    {
+                                        ExtractFullPath = true,
+                                        Overwrite = Overwrite
+                                    });
+                                }
+                                TempData.Add(entry.Key, entry);
+                            }
+                        }
+                        else
+                        {
+                            foreach (SharpCompress.Archives.IArchiveEntry entry in archive.Entries)
+                            {
+                                TempData.Add(entry.Key, entry);
+                            }
+                        }
+                    }
+                    return TempData;
+                }
+            }
         }
         /// <summary>
         /// 网络相关
@@ -163,45 +221,167 @@ namespace 小工具集
             /// <param name="Url">地址</param>
             /// <param name="Path">保存路径</param>
             /// <param name="Name">保存文件名</param>
+            /// <param name="progress">下载进度</param>
             /// <returns>文件路径</returns>
-            public static async Task<string?> DownloadFile(string Url, string? Path = null, string? Name = null)
+            public static async Task<string?> DownloadFile(string Url, string? Path = null, string? Name = null, IProgress<double>? progress = null)
             {
                 Path ??= Windows.Path.ThisAppPath;
                 using (HttpClient client = new HttpClient())
                 {
                     try
                     {
-                        // 发送GET请求并获取响应
-                        HttpResponseMessage response = await client.GetAsync(Url);
-
-                        // 确保响应成功
-                        response.EnsureSuccessStatusCode();
-
-                        // 从响应中获取文件名
-                        Name ??= response.Content.Headers.ContentDisposition?.FileName ?? System.IO.Path.GetFileName(new Uri(Url).LocalPath);
-
-                        //过滤文件名
-                        string invalidChars = new string(System.IO.Path.GetInvalidFileNameChars());
-                        Name = string.Concat(Name.Split(invalidChars.ToCharArray()));
-
-                        Name = System.IO.Path.Combine(Path, Name);
-                        // 读取响应内容并保存到文件
-                        using (Stream contentStream = await response.Content.ReadAsStreamAsync())
+                        using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, Url))
+                        using (HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
                         {
-                            using (FileStream fileStream = new FileStream(Name, FileMode.Create, FileAccess.Write, FileShare.None))
+                            // 确保响应成功
+                            response.EnsureSuccessStatusCode();
+
+                            // 从响应中获取文件名
+                            Name ??= response.Content.Headers.ContentDisposition?.FileName ?? System.IO.Path.GetFileName(new Uri(Url).LocalPath);
+
+                            // 过滤文件名
+                            string invalidChars = new string(System.IO.Path.GetInvalidFileNameChars());
+                            Name = string.Concat(Name.Split(invalidChars.ToCharArray()));
+
+                            Name = System.IO.Path.Combine(Path, Name);
+
+                            // 获取响应内容流
+                            using (Stream contentStream = await response.Content.ReadAsStreamAsync())
                             {
-                                await contentStream.CopyToAsync(fileStream);
+                                // 获取文件总大小
+                                long? totalBytes = response.Content.Headers.ContentLength;
+
+                                // 创建文件流
+                                using (FileStream fileStream = new FileStream(Name, FileMode.Create, FileAccess.Write, FileShare.None))
+                                {
+                                    byte[] buffer = new byte[4096];
+                                    long bytesCopied = 0;
+                                    int bytesRead;
+                                    while ((bytesRead = await contentStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                                    {
+                                        await fileStream.WriteAsync(buffer, 0, bytesRead);
+
+                                        bytesCopied += bytesRead;
+                                        if (totalBytes.HasValue && progress != null)
+                                        {
+                                            // 使用已下载的大小计算进度
+                                            double percentage = (double)bytesCopied / totalBytes.Value * 100;
+                                            progress.Report(percentage);
+                                        }
+                                    }
+                                }
                             }
+
+                            return Name;
                         }
                     }
                     catch (Exception ex)
                     {
                         Console.WriteLine(ex.Message);
-                        return null;
+                        // 可以根据实际需求进行异常处理，返回更详细的错误信息给调用者
+                        throw new Exception("下载文件失败：" + ex.Message);
                     }
-                    return Name;
                 }
             }
+            /// <summary>
+            /// 带有进度条的下在文件
+            /// </summary>
+            /// <param name="Url">文件地址</param>
+            /// <param name="Path">保存路径</param>
+            /// <param name="Name">保存文件名</param>
+            /// <returns>保存文件路径</returns>
+            public static async Task<string?> DownloadFileWithProgress(string Url, string? Path = null, string? Name = null)
+            {
+                Path ??= Windows.Path.ThisAppPath;
+
+                // 发送HTTP请求并获取文件总大小
+                long? totalBytes = await GetFileSizeAsync(Url);
+
+                // 创建进度报告对象
+                ProgressTracker progressTracker = new ProgressTracker(totalBytes);
+
+                // 创建进度报告回调函数
+                IProgress<double> progress = new Progress<double>(percentage =>
+                {
+                    progressTracker.UpdateProgress(percentage);
+                    long? downloadedBytes = progressTracker.DownloadedBytes;
+                    double speed = progressTracker.DownloadSpeed;
+
+                    // 显示下载进度、已下载大小和下载速度
+                    Console.WriteLine($"下载进度：{percentage.ToString("0.0")}% - {GetSizeString(downloadedBytes)}/{GetSizeString(totalBytes)} - {GetSizeString((long?)speed)}/s");
+                });
+
+                // 下载文件并返回文件路径
+                return await DownloadFile(Url, Path, Name, progress);
+            }
+            private static async Task<long?> GetFileSizeAsync(string Url)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Head, Url))
+                    using (HttpResponseMessage response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        response.EnsureSuccessStatusCode();
+                        return response.Content.Headers.ContentLength;
+                    }
+                }
+            }
+            private static string GetSizeString(long? size)
+            {
+                if (size == null || size <= 0)
+                    return "0B";
+
+                string[] suffixes = { "B", "KB", "MB", "GB", "TB" };
+                int suffixIndex = 0;
+                double bytes = (double)size.Value;
+
+                while (bytes >= 1024 && suffixIndex < suffixes.Length - 1)
+                {
+                    bytes /= 1024;
+                    suffixIndex++;
+                }
+
+                return $"{bytes:F1}{suffixes[suffixIndex]}";
+            }
+            class ProgressTracker
+            {
+                private readonly long? totalBytes;
+                private long downloadedBytes;
+                private Stopwatch stopwatch;
+
+                public ProgressTracker(long? totalBytes)
+                {
+                    this.totalBytes = totalBytes;
+                    downloadedBytes = 0;
+                    stopwatch = Stopwatch.StartNew();
+                }
+
+                public long? DownloadedBytes => downloadedBytes;
+
+                public double DownloadSpeed
+                {
+                    get
+                    {
+                        double elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+                        if (elapsedSeconds > 0 && downloadedBytes > 0)
+                        {
+                            double speed = downloadedBytes / elapsedSeconds;
+                            return speed;
+                        }
+
+                        return 0;
+                    }
+                }
+
+                public void UpdateProgress(double percentage)
+                {
+#pragma warning disable CS8629 // 可为 null 的值类型可为 null。
+                    downloadedBytes = (long)(totalBytes * (percentage / 100));
+#pragma warning restore CS8629 // 可为 null 的值类型可为 null。
+                }
+            }
+
+
             /// <summary>
             /// 获取所有网卡的IP
             /// </summary>
@@ -241,7 +421,13 @@ namespace 小工具集
             /// </summary>
             public class IpAddress
             {
+                /// <summary>
+                /// IPV4
+                /// </summary>
                 public string? IPV4 { get; set; }
+                /// <summary>
+                /// IPV6
+                /// </summary>
                 public string? IPV6 { get; set; }
             }
             /// <summary>
@@ -262,6 +448,70 @@ namespace 小工具集
         public static class User
         {
 
+        }
+        /// <summary>
+        /// 文本相关
+        /// </summary>
+        public static class Text
+        {
+            /// <summary>
+            /// 自动转换单位
+            /// </summary>
+            /// <param name="bytes">字节</param>
+            /// <param name="manualUnit">指定单位(未指定自动转换)</param>
+            /// <param name="decimalPlaces">小数点位数</param>
+            /// <returns>转换后字符串</returns>
+            public static string ConvertByteUnits(double bytes, string manualUnit = "", int decimalPlaces = 2)
+            {
+                double convertedValue = bytes;
+                string convertedUnit = "B";
+
+                // 根据手动指定的单位进行转换
+                if (!string.IsNullOrEmpty(manualUnit))
+                {
+                    switch (manualUnit.ToLower())
+                    {
+                        case "kb":
+                            convertedValue /= 1024;
+                            convertedUnit = "KB";
+                            break;
+                        case "mb":
+                            convertedValue /= Math.Pow(1024, 2);
+                            convertedUnit = "MB";
+                            break;
+                        case "gb":
+                            convertedValue /= Math.Pow(1024, 3);
+                            convertedUnit = "GB";
+                            break;
+                        case "tb":
+                            convertedValue /= Math.Pow(1024, 4);
+                            convertedUnit = "TB";
+                            break;
+                        default:
+                            throw new ArgumentException("无效的单位");
+                    }
+
+                    return Math.Round(convertedValue, decimalPlaces).ToString() + " " + convertedUnit;
+                }
+
+                // 自动转换为推荐单位
+                string[] units = { "B", "KB", "MB", "GB", "TB" };
+                int unitIndex = 0;
+                while (convertedValue >= 1024 && unitIndex < units.Length - 1)
+                {
+                    convertedValue /= 1024;
+                    unitIndex++;
+                }
+
+                if (convertedValue < 0.01 && unitIndex > 0)
+                {
+                    // 当转换后的值小于 0.01 且不是最小单位时，将单位回退一个级别
+                    convertedValue *= 1024;
+                    unitIndex--;
+                }
+
+                return Math.Round(convertedValue, decimalPlaces).ToString() + " " + units[unitIndex];
+            }
         }
     }
 }
--- 工具测试/Program.cs
+++ 工具测试/Program.cs
@@ -10,8 +10,8 @@ using static 小工具集.Windows.File;
     Console.WriteLine(Data);
 }*/
 //Console.WriteLine(小工具集.Windows.Path.ThisAppPath);
-
-//Console.WriteLine(await DownloadFile("https://d-ssl.dtstatic.com/uploads/blog/202109/24/20210924081215_63853.thumb.300_0.jpeg_webp"));
+//Console.WriteLine(await Network.DownloadFileWithProgress("https://d-ssl.dtstatic.com/uploads/blog/202109/24/20210924081215_63853.thumb.300_0.jpeg_webp"));
+//Console.WriteLine(await Network.DownloadFileWithProgress("https://az764295.vo.msecnd.net/stable/660393deaaa6d1996740ff4880f1bad43768c814/VSCodeUserSetup-x64-1.80.0.exe"));
 
 /*foreach (var Data in Network.GetIpAddress())
 { 
@@ -19,3 +19,6 @@ using static 小工具集.Windows.File;
 }*/
 
 //Console.WriteLine(Network.GetPublicIP());
+//Unzip(小工具集.Windows.Path.ThisAppPath+ "/快易享-Webp.rar", 小工具集.Windows.Path.ThisAppPath+"/A");
+//Unzip(小工具集.Windows.Path.ThisAppPath + "/快易享-Webp.rar");
+
